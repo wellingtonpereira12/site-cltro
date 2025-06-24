9e4fcbd7506a9a53d96d30ee171eb68a94f8e79a
Edison Matos <edison.matos@live.com>
2025-06-24 07:14:02 -0300

    chore: Cria Teste.jsx e insere tailwindplus navbar em componente separado

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles.css";
+import Teste from "./Teste";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Teste />
   </React.StrictMode>
 );
