a47b96fb274e796eda535aa2ea162ee282a26ec9
Edison Matos <edison.matos@live.com>
2025-07-05 09:55:05 -0300

    feat: Separa navbar do App.jsx

---
+++ src/NavMenu.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useAuth } from "./context/AuthContext";
+
+export default function NavMenu() {
+  const { user, logout } = useAuth();
+
+  return (
+    <nav className="nav-menu">
+      <Link to="/">Home</Link>
+      <Link to="/criar-conta">Crie sua conta</Link>
+      <Link to="/download">Download</Link>
+      {user ? (
+        <>
+          <Link to="/perfil">Perfil</Link>
+          <button onClick={logout} className="logout-button">
+            Sair
+          </button>
+        </>
+      ) : (
+        <Link to="/login">Login</Link>
+      )}
+      <a
+        href="https://discord.gg/6qX5VEKakP"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Discord
+      </a>
+      <a
+        href="https://cltro.gitbook.io/cltro/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Wiki
+      </a>
+      <span className="bg-red-500">Teste</span>
+    </nav>
+  );
+}
